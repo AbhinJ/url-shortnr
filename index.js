47e9e46560398438f9621ef8000f4a6f1e949c83
Abhinav <hemrajjain1999@gmail.com>
2018-07-01 18:05:43 +0530

    created boilerplate

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
