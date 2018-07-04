ce5eb4f6240752085fb1fa7e2d8724833c821169
Abhinav <hemrajjain1999@gmail.com>
2018-07-04 14:18:35 +0530

    For production

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./routes")(app);
-const PORT = process.env.PORT || 5000;
+if ((process.env.NODE_ENV = "production")) {
+  const path = require("path");
+  app.use(express.static(path.join(__dirname, "/views/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views", "build", "index.html"));
+  });
+}
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT}`);
 });
